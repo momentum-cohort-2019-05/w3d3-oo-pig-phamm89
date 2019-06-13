af70553041a23057e20941d92791a36ca1d2f989
Michael <phamm89@gmail.com>
2019-06-13 11:08:51 -0400

    OOPig

--- pig_test.py
+++ pig_test.py
@@ -1,7 +1,16 @@
 import pytest
 
-from oopig import Dice
+from oopig import Dice, Player
 
 def test_dice_roll():
     dice = Dice()
     assert (dice.roll() in range(1, 7))
+
+def test_name():
+    player1 = Player('Michael')
+    assert player1.name == "Michael"
+
+def test_add_score():
+    player1 = Player('Michael')
+    player_score = 5
+    Player.add_score(player1, player_score) == 5
